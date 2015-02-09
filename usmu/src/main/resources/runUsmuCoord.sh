4c5890f1248f16a4875988424c5967957ba38cb1
Shinta Smith <shinta.smith@RACKSPACE.COM>
2015-02-09 09:36:39 -0600

    comment runUsmuCoord.sh

--- usmu/src/main/resources/runUsmuCoord.sh
+++ usmu/src/main/resources/runUsmuCoord.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
+#
+# This script is used to deploy new versions of Usmu's Oozie Coordinator and Workflow.
 
-#USMU_DIR=/etc/cloudfeeds-nabu/usmu
-USMU_DIR=./
+# TODO: this probably needs to be externalized somewhere
+USMU_DIR=/etc/cloudfeeds-nabu/usmu
 REGION_LIST="DFW ORD IAD SYD LON HKG"
 OOZIE=oozie
 OOZIE_URL=http://cloudfeeds-visual-n02.test.ord1.ci.rackspace.net:11000/oozie
-#ERROR_EMAIL=cloudfeeds@rackspace.com
-ERROR_EMAIL=shinta.smith@rackspace.com
+ERROR_EMAIL=cloudfeeds@rackspace.com
+
+# There doesn't seem to be a nicer way to find existing Oozie jobs so
+# we can kill and submit a new set. This one I found on the web and
+# probably needs to be evaluated every time we upgrade Oozie.
 
 # clean up previous run
-curl 'http://localhost:11000/oozie/v1/jobs?len=1000&filter=status%3DRUNNING&jobtype=coord'  | python -mjson.tool | grep "coordJobId" | sed "s/\(.*\)coordJobId\(.*\): \"\(.*\)\"\(.*\)/\3/" | while read job_id; do oozie job -oozie ${OOZIE_URL} -kill $job_id; done
+curl '${OOZIE_URL}/v1/jobs?len=1000&filter=status%3DRUNNING&jobtype=coord'  | python -mjson.tool | grep "coordJobId" | sed "s/\(.*\)coordJobId\(.*\): \"\(.*\)\"\(.*\)/\3/" | while read job_id; do oozie job -oozie ${OOZIE_URL} -kill $job_id; done
 
 # run the feedsImport-coord.xml for each region
 for aRegion in ${REGION_LIST}
