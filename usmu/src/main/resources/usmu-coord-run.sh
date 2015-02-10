a875cda216b228294a3de0386a5ed3597aac0780
Shinta Smith <shinta.smith@RACKSPACE.COM>
2015-02-10 11:14:54 -0600

    did the RPM, clean up deploy/install scripts

--- usmu/src/main/resources/usmu-coord-run.sh
+++ usmu/src/main/resources/usmu-coord-run.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 #
 # This script is used to deploy new versions of Usmu's Oozie Coordinator and Workflow.
+# It is a temporary script used during development, when we're updating coordinators
+# and workflows. This eventually needs to be part of the deployment script (Ansible,
+# etc)
+#
 
-# TODO: this probably needs to be externalized somewhere
-USMU_DIR=/etc/cloudfeeds-nabu/usmu
+usage()
+{
+    echo "Usage: `basename $0` <startTime> <endTime>"
+    echo "where:"
+    echo "  <startTime>   is the start time of when Oozie should materialize jobs. Format: yyyy-MM-ddThh:mmZ"
+    echo "  <endTime>     is the end time of when Oozie should stop materializing jobs. Same format as above"
+    exit 1
+}
+
+if [ $# -lt 2 ]; then
+    echo "Missing argument to script"
+    usage
+fi
+
+START_TIME=$1
+END_TIME=$2
+
+# TODO: these probably needs to be externalized somewhere
+USMU_ETC_DIR=/etc/cloudfeeds-nabu/usmu
 REGION_LIST="DFW ORD IAD SYD LON HKG"
 OOZIE=oozie
 OOZIE_URL=http://cloudfeeds-visual-n02.test.ord1.ci.rackspace.net:11000/oozie
@@ -14,10 +35,12 @@ ERROR_EMAIL=cloudfeeds@rackspace.com
 # probably needs to be evaluated every time we upgrade Oozie.
 
 # clean up previous run
-curl '${OOZIE_URL}/v1/jobs?len=1000&filter=status%3DRUNNING&jobtype=coord'  | python -mjson.tool | grep "coordJobId" | sed "s/\(.*\)coordJobId\(.*\): \"\(.*\)\"\(.*\)/\3/" | while read job_id; do oozie job -oozie ${OOZIE_URL} -kill $job_id; done
+curl ${OOZIE_URL}'/v1/jobs?len=1000&filter=status%3DRUNNING&jobtype=coord'  | python -mjson.tool | grep "coordJobId" | sed "s/\(.*\)coordJobId\(.*\): \"\(.*\)\"\(.*\)/\3/" | while read job_id; do oozie job -oozie ${OOZIE_URL} -kill $job_id; done
 
 # run the feedsImport-coord.xml for each region
 for aRegion in ${REGION_LIST}
 do
-    $OOZIE job -oozie ${OOZIE_URL} -config ${USMU_DIR}/feedsImport-coord.properties -submit -Dregion=$aRegion -DemailToAddress="$ERROR_EMAIL" -DstartTime="2015-02-04T00:00Z" -DendTime="2025-12-31T00:00Z"
+    $OOZIE job -oozie ${OOZIE_URL} -config ${USMU_ETC_DIR}/feedsImport-coord.properties -submit \
+        -Dregion=$aRegion -DemailToAddress="${ERROR_EMAIL}" \
+        -DstartTime="${START_TIME}" -DendTime="${END_TIME}"
 done
--- usmu/src/main/resources/usmu-deploy.sh
+++ usmu/src/main/resources/usmu-deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 #
 # This script is used to copy necessary Usmu Coordinator and Workflow files into
-# HDFS
+# HDFS. Eventually this needs to be part of the deployment scripts (Ansible, etc).
+# 
 
 HDFS_DEPLOY_DIR=/cloudfeeds/usmu
-USMU_LOCAL_DIR=/opt/cloudfeeds-nabu/usmu
+USMU_INSTALL_DIR=`dirname $0`/..
 
 hadoop fs -mkdir -p ${HDFS_DEPLOY_DIR}/
 
-# this should copy 
-hadoop fs -copyFromLocal -f ${USMU_LOCAL_DIR}/*.xml ${USMU_LOCAL_DIR}/*.q ${HDFS_DEPLOY_DIR}/
+#
+# This assumes a certain directory layout as laid out in the RPM.
+# The scripts will be installed under $USMU_INSTALL_DIR/bin.
+# The necessary files are installed directly under $USMU_INSTALL_DIR.
+# If you change the RPM, this probably needs to change too.
+#
+hadoop fs -copyFromLocal -f ${USMU_INSTALL_DIR}/*.xml ${USMU_INSTALL_DIR}/*.q ${HDFS_DEPLOY_DIR}/
