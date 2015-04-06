d09fbe6111f8e8f7e35713a1467474a5ae9b3567
Greg Sharek <gregsharek@rackspace.com>
2015-04-06 11:23:35 -0500

    adding link for tiamat.sh; fixing build script jar build

--- tiamat/src/main/resources/tiamat.sh
+++ tiamat/src/main/resources/tiamat.sh
@@ -3,4 +3,4 @@
 SPARK_HOME=/usr/hdp/2.2.0.0-2041/spark
 TIAMAT_HOME=/opt/cloudfeeds-nabu/tiamat
 
-${SPARK_HOME}/bin/spark-submit --class com.rackspace.feeds.archives.Tiamat --master yarn-client ${TIAMAT_HOME}/lib/cloudfeeds-nabu-tiamat-*.jar "$@"
+${SPARK_HOME}/bin/spark-submit --class com.rackspace.feeds.archives.Tiamat --master yarn-client ${TIAMAT_HOME}/lib/cloudfeeds-nabu-tiamat.jar "$@"
