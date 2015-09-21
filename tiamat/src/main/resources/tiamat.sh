b6c066654913b9ca6f7adc78105dc8f60bda8965
Chandra Addala <chandra.addala@rackspace.com>
2015-09-21 17:21:52 -0500

    [CF-1039] Updating tiamat.sh to use spark-submit from /usr/bin

--- tiamat/src/main/resources/tiamat.sh
+++ tiamat/src/main/resources/tiamat.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
-SPARK_HOME=/usr/hdp/2.2.0.0-2041/spark
+ 
 TIAMAT_HOME=/opt/cloudfeeds-nabu/tiamat
-
-${SPARK_HOME}/bin/spark-submit --class com.rackspace.feeds.archives.Tiamat \
-   --driver-java-options "-Dlog4j.configuration=file:///etc/cloudfeeds-nabu/tiamat/log4j.properties" \
+ 
+/usr/bin/spark-submit --verbose --class com.rackspace.feeds.archives.Tiamat \
+   --files /etc/cloudfeeds-nabu/tiamat/log4j.properties \
+   --driver-java-options "-Dlog4j.configuration=file:///etc/cloudfeeds-nabu/tiamat/log4j-local.properties" \
    --master yarn-client ${TIAMAT_HOME}/lib/cloudfeeds-nabu-tiamat.jar "$@"
